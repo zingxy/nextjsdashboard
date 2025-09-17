3aa2d0de54670f5c9e6a96ed2cf596c13c64e780
zingxy <1067577595@qq.com>
2025-09-18 00:26:51 +0800

    init commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+import '@/app/ui/global.css';
+import { inter, firaCode } from '@/app/ui/fonts';
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +8,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${firaCode.className}`}>
+        <div>{children}</div>
+      </body>
     </html>
   );
 }
---
+++ app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Inter, Fira_Code, Lusitana } from 'next/font/google';
+
+export const inter = Inter({
+  subsets: ['latin'],
+});
+
+export const firaCode = Fira_Code();
+
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+});
