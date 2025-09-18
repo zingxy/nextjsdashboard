6ec2ab05533c28fc132b069b50a54843af5be1f0
zingxy <1067577595@qq.com>
2025-09-18 22:35:00 +0800

    fix: remove some unused variables

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import '@/app/ui/global.css';
-import { inter, firaCode } from '@/app/ui/fonts';
+import { firaCode } from '@/app/ui/fonts';
 
 export default function RootLayout({
   children,
--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,11 +1,14 @@
-import { Inter, Fira_Code, Lusitana } from 'next/font/google';
+import { Inter, Lusitana, Fira_Code } from 'next/font/google';
 
 export const inter = Inter({
   subsets: ['latin'],
 });
-
-export const firaCode = Fira_Code();
+export const firaCode = Fira_Code({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
 
 export const lusitana = Lusitana({
   weight: ['400', '700'],
+  subsets: ['latin'],
 });
