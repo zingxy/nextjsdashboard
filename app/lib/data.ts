74a37686bd2449ffc47da9031ca193ade9e1a62b
zingxy <1067577595@qq.com>
2025-09-19 08:42:32 +0800

    feat: setup database

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -21,7 +21,7 @@ export async function fetchRevenue() {
 
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
 
-    // console.log('Data fetch completed after 3 seconds.');
+    console.log('Data fetch completed after 3 seconds.', data);
 
     return data;
   } catch (error) {
